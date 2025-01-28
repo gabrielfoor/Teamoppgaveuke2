7b5fe157076fe02372ead349e2257a865fd8c145
gabrielfoor <78699691+gabrielfoor@users.noreply.github.com>
2025-01-28 15:07:15 +0100

    dag 1

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 
-
-let items = []
-
+// items er en array der src til bilder ligger
+const items = []
+// inventory er der plasseringen i inventory ligger
 let inventory = []
-
+// gridSlot er der plasseringen utenfor inventory ligger
 let gridSlot = []
 
 
-function inventoryAdd(item, slot) {
-    inventory.push(item);
+// function inventoryAdd(item, slot) {
+//     inventory.push(item)
+//     gridSlot.push(slot
+// }
+// function inventoryRemove(item, slot) {
+//     inventory.pop(item)
+//     gridSlot.pop(slot)
+// }
+let imgToShow = "";
 
-    gridSlot.push(slot)
-
-}
-function inventoryRemove(item, slot) {
-    inventory.pop(item);
-
-    gridSlot.pop(slot)
-
+//Henter item sin src fra items array og legger inn i inventory eller gridSlot
+function changeImg(imageSource) {
+    imgToShow = imageSource
+    document.getElementById('').innerHTML = /*HTML*/ `
+    <img src= ${imgToShow} alt=""> <br>`; 
 }
